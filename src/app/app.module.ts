a097a73bb475099ee001de6323be0fa6e7f760f7
sandeep924 <143247470+sandeep924@users.noreply.github.com>
2023-08-31 21:48:08 +0530

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { PostListingComponent } from './post-list/post-list.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    CreatePostComponent,
+    PostListingComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-postlist',
+  templateUrl:'./post-list.component.html',
+  styleUrls:['./post-list.component.css']
+})
+export class PostListingComponent{
+
+}
