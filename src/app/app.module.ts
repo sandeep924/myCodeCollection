d9a318c498e4b1ce194c98a6fbfe4113b8c41760
sandeep924 <143247470+sandeep924@users.noreply.github.com>
2023-08-31 23:46:58 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,31 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { PostListingComponent } from './post-list/post-list.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header/header-component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule,
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
     CreatePostComponent,
     PostListingComponent,
+    HeaderComponent,
   ],
   bootstrap: [AppComponent],
 })
--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css'],
 })
-export class CreatePostComponent {}
+export class CreatePostComponent implements OnInit { 
+ @Output() createPostValue=new EventEmitter();
+  public contMsgValue: string = '';
+public contTitleValue:string='';
+  ngOnInit(): void {}
+  addPostmessege(input) {
+    const post={title:this.contTitleValue,content:this.contMsgValue}
+    this.createPostValue.emit(post)
+  }
+}
