284cf91a9598d3a33364d3220a3609af12f9ec40
sandeep924 <143247470+sandeep924@users.noreply.github.com>
2023-09-25 09:50:26 +0530

    2 files modified, 1 added

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -5,13 +5,15 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.css'],
 })
-export class CreatePostComponent implements OnInit { 
- @Output() createPostValue=new EventEmitter();
+export class CreatePostComponent implements OnInit {
+  @Output() createPostValue = new EventEmitter();
   public contMsgValue: string = '';
-public contTitleValue:string='';
+  public contTitleValue: string = '';
   ngOnInit(): void {}
   addPostmessege(input) {
-    const post={title:this.contTitleValue,content:this.contMsgValue}
-    this.createPostValue.emit(post)
+    if (this.contTitleValue) {
+      const post = { title: this.contTitleValue, content: this.contMsgValue };
+      this.createPostValue.emit(post);
+    }
   }
 }
